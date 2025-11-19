b71e85637b93bc7ba898c072d07e92f41d4c7958
Najmi Afiq <najmi.afiq1102@gmail.com>
2025-11-19 14:33:41 +0800

    created base game and the mechanics

--- js/index.js
+++ js/index.js
@@ -6,3 +6,163 @@ canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+const gravity = 0.7
+class Sprite {
+    constructor({ position, velocity, color }) {
+        this.position = position,
+        this.velocity = velocity,
+        this.height = 150,
+        this.lastKey,
+        this.attackHBox = {
+            position: this.position,
+            width: 100,
+            height: 50
+        },
+        this.color = color
+    }
+
+    draw() {
+        c.fillStyle = this.color;
+        c.fillRect(this.position.x, this.position.y, 50, this.height);
+
+        //attack box
+        c.fillStyle = 'green';
+        c.fillRect(this.attackHBox.position.x, this.attackHBox.position.y, this.attackHBox.width, this.attackHBox.height);
+    }
+
+    update() {
+        this.draw();
+        
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0;
+        }else this.velocity.y += gravity;
+    }
+}
+
+const player1 = new Sprite({
+    position: {
+    x: 0,
+    y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    },
+    color: 'red'
+})
+
+player1.draw();
+
+const player2 = new Sprite({
+    position: {
+    x: 800,
+    y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    },
+    color: 'blue'
+})
+
+player2.draw();
+
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    w: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    },
+    ArrowUp: {
+        pressed: false
+    }
+}
+
+
+function animate() {
+    window.requestAnimationFrame(animate);
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    player1.update();
+    player2.update();
+
+    player1.velocity.x = 0;
+    player2.velocity.x = 0;
+
+    //player1 movement
+    if (keys.a.pressed && player1.lastKey === 'a') {
+        player1.velocity.x = -5;
+    } else if (keys.d.pressed && player1.lastKey === 'd') {
+        player1.velocity.x = 5;
+    }
+
+    //player2 movement
+    if (keys.ArrowLeft.pressed && player2.lastKey === 'ArrowLeft') {
+        player2.velocity.x = -5;
+    } else if (keys.ArrowRight.pressed && player2.lastKey === 'ArrowRight') {
+        player2.velocity.x = 5;
+    }
+
+    if (player1.attackHBox.position.x + player1.attackHBox.width >= player2.position.x) {
+        
+    }
+}
+
+animate();
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = true;
+            player1.lastKey = 'd'
+            break;
+        case 'a':
+            keys.a.pressed = true;
+            player1.lastKey = 'a';
+            break;
+        case 'w':
+            player1.velocity.y = -20;
+            break;
+        
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true;
+            player2.lastKey = 'ArrowRight';
+            break;
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true;
+            player2.lastKey = 'ArrowLeft';
+            break;
+        case 'ArrowUp':
+            player2.velocity.y = -20;
+            break;
+    }
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = false;
+            break;
+        case 'a':
+            keys.a.pressed = false;
+            break;
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false;
+            break;
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false;
+            break;
+    }
+})
